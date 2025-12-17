b493192e1cf20177a5785d3b451ebf916caa4620
r h <ravanhawrami@gmail.com>
2025-12-17 02:30:04 -0600

    parallel appends

--- ox/compute/str_ops.go
+++ ox/compute/str_ops.go
@@ -1,1 +1,49 @@
 package compute
+
+import (
+	"sync"
+
+	"github.com/rhawrami/ox-frame/ox/vector"
+)
+
+func AppendLit(x *vector.StringVector, lit string, opAppend func(outDat, xDat []byte, outOff, xOff []int64, lit []byte)) *vector.StringVector {
+	litB := []byte(lit)
+	newLenB := len(x.Data()) + (len(x.Data()) * len(litB))
+
+	newValidityMapBuffer := x.Validity().DeepCopyBuff() // will stay same
+	newOffsetsBuffer := make([]int64, len(x.Offsets()))
+	newDataBuffer := make([]byte, newLenB)
+
+	chunkSize := x.Len()/NumWorkers + 1
+	if x.Len()%NumWorkers == 0 {
+		chunkSize = chunkSize - 1
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(NumWorkers)
+	for i := 0; i < NumWorkers; i++ {
+		go func(i int) {
+			defer wg.Done()
+
+			startElement, endElement := i*chunkSize, i*chunkSize+chunkSize
+
+			opAppend(
+				newDataBuffer,
+				x.Data(),
+				newOffsetsBuffer[startElement:endElement],
+				x.Offsets()[startElement:endElement],
+				litB,
+			)
+
+		}(i)
+	}
+	wg.Wait()
+
+	return &vector.StringVector{}
+}
+
+func appendRight(outD, xD []byte, outO, xO []int64, lit []byte) {
+	for i := 0; i < len(outO); i++ {
+		newOffSet := 0
+	}
+}
